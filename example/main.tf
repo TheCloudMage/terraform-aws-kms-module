c470b50c37587276a68ae358f94f0e9987a59455
Rich Nason <rnason@cloudmage.io>
2020-04-02 02:42:25 -0600

    Added Test Cases to example to test optional variables

--- example/main.tf
+++ example/main.tf
@@ -39,21 +39,52 @@ data "aws_iam_policy_document" "custom_policy" {
 #################
 # KMS CMK Module
 #################
-module "demo_cmk" {
+# Test Defaults
+module "default_config" {
   source = "../"
   # source = "git@github.com:CloudMage-TF/AWS-KMS-Module.git?ref=v1.0.5"
 
-  // Required Variable Example
   name        = var.name
   description = var.description
+  
+  tags  = var.tags
+}
 
-  // Optional Variable Example
-  # policy        = data.aws_iam_policy_document.custom_policy.json
-  # key_owners    = var.key_owners
-  # key_admins    = var.key_admins
-  # key_users     = var.key_users
-  # key_grantees  = var.key_grantees
-  # key_grant_resource_restriction = var.key_grant_resource_restriction
-  # key_grant_resource_restriction = false
-  # tags           = var.tags
+# Test Custom Policy
+module "custom_policy" {
+  source = "../"
+  # source = "git@github.com:CloudMage-TF/AWS-KMS-Module.git?ref=v1.0.5"
+
+  name                     = var.name
+  description              = var.description
+  enable_key_rotation      = var.enable_key_rotation
+  deletion_window_in_days  = var.deletion_window_in_days
+  key_usage                = var.key_usage
+  customer_master_key_spec = var.customer_master_key_spec
+  policy                   = data.aws_iam_policy_document.custom_policy.json
+}
+
+# Test Key Policy Generation
+module "key_policy_condition" {
+  source = "../"
+  # source = "git@github.com:CloudMage-TF/AWS-KMS-Module.git?ref=v1.0.5"
+
+  name         = var.name
+  description  = var.description
+  key_owners   = var.key_owners
+  key_admins   = var.key_admins
+  key_grantees = var.key_grantees
+}
+
+# Test Key Policy Generation
+module "key_policy_no_condition" {
+  source = "../"
+  # source = "git@github.com:CloudMage-TF/AWS-KMS-Module.git?ref=v1.0.5"
+
+  name         = var.name
+  description  = var.description
+  key_users    = ["arn:aws:iam::123456789101:user/TPol"]
+  key_grantees = var.key_grantees
+  
+  key_grant_resource_restriction = var.key_grant_resource_restriction
 }
